93cd9468a08d7f6fac1561270010bbe3b0856952
PritrirajPartho <dapartho5616@gmail.com>
2023-05-03 22:17:38 +0600

    halka-added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+// -------------------------
+const chefs = require('./data/chefs.json');
 
+//Example part start to here........
 app.get('/', (req, res) => {
   res.send('Chef-Recipe-assignment')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+// data using is start here bro......
+app.get('/chefs', (req, res) =>{
+   res.send(chefs);
 })
